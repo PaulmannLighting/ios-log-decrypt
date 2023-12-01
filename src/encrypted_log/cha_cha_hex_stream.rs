7afa11ae9befc9b2133041407c50b7ab2ee52dea
Richard Neumann <neumann@paulmann.de>
2023-12-01 11:22:38 +0100

    Parse blocks in iterator

--- src/encrypted_log/cha_cha_hex_stream.rs
+++ src/encrypted_log/cha_cha_hex_stream.rs
@@ -2,11 +2,14 @@ mod block;
 
 use block::Block;
 use chacha20poly1305::{ChaCha20Poly1305, KeyInit};
-use hex::FromHex;
+use hex::{FromHex, FromHexError};
+use std::iter::Map;
 use std::str::SplitWhitespace;
 
+type Blocks<'a> = Map<SplitWhitespace<'a>, fn(&str) -> Result<Block, FromHexError>>;
+
 pub struct ChaChaHexStream<'a> {
-    blocks: SplitWhitespace<'a>,
+    blocks: Blocks<'a>,
     cipher: ChaCha20Poly1305,
 }
 
@@ -14,7 +17,9 @@ impl<'a> ChaChaHexStream<'a> {
     #[must_use]
     pub fn new(blocks: &'a str, key: &[u8]) -> Self {
         Self {
-            blocks: blocks.split_whitespace(),
+            blocks: blocks
+                .split_whitespace()
+                .map(|block| Block::from_hex(block)),
             cipher: ChaCha20Poly1305::new(key.into()),
         }
     }
@@ -26,6 +31,6 @@ impl<'a> Iterator for ChaChaHexStream<'a> {
     fn next(&mut self) -> Option<Self::Item> {
         self.blocks
             .next()
-            .map(|block| Ok(Block::from_hex(block)?.decrypt(&self.cipher)?))
+            .map(|block| Ok(block?.decrypt(&self.cipher)?))
     }
 }
