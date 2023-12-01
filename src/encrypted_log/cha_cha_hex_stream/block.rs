7ecaefa34632fb004d0a2b9d5b32ac4a6e0e5eb2
Richard Neumann <neumann@paulmann.de>
2023-12-01 11:35:33 +0100

    Store block bytes as boxed slice

--- src/encrypted_log/cha_cha_hex_stream/block.rs
+++ src/encrypted_log/cha_cha_hex_stream/block.rs
@@ -5,7 +5,7 @@ use hex::{FromHex, FromHexError};
 const NONCE_SIZE: usize = 12;
 
 #[derive(Debug)]
-pub struct Block(Vec<u8>);
+pub struct Block(Box<[u8]>);
 
 impl Block {
     pub fn nonce(&self) -> &[u8] {
@@ -28,6 +28,6 @@ impl FromHex for Block {
     where
         T: AsRef<[u8]>,
     {
-        Vec::<u8>::from_hex(hex).map(Self)
+        Vec::<u8>::from_hex(hex).map(|bytes| Self(bytes.into_boxed_slice()))
     }
 }
