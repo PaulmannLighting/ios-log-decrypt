f1d3c9c7667c468fc0a4b9f9e5fe3db0420f8a18
Richard Neumann <neumann@paulmann.de>
2023-12-05 14:28:40 +0100

    Use clap_stdin::FileOrStdin

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use clap::Parser;
+use clap_stdin::FileOrStdin;
 use ios_log_decrypt::EncryptedLog;
 use log::error;
-use std::fs::read_to_string;
 use std::io::{stdout, Write};
-use std::path::PathBuf;
 use std::process::exit;
 
 #[derive(Debug, Parser)]
 struct Args {
     #[arg(index = 1, help = "path to the encrypted log file")]
-    filename: PathBuf,
+    logfile: FileOrStdin,
     #[arg(long, short, help = "hexadecimal decryption key")]
     key: String,
 }
@@ -18,10 +17,7 @@ fn main() {
     env_logger::init();
 
     let args = Args::parse();
-    let encrypted_log = EncryptedLog::new(read_to_string(&args.filename).unwrap_or_else(|error| {
-        error!("{error}");
-        exit(1);
-    }));
+    let encrypted_log = EncryptedLog::new(args.logfile.to_string());
     let key = hex::decode(&args.key).unwrap_or_else(|error| {
         error!("{error}");
         exit(2);
