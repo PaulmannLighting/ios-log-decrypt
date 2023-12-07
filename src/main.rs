b52a7c8b3ea93d505b5e75968234701980b78064
Richard Neumann <neumann@paulmann.de>
2023-12-07 08:05:06 +0100

    Use generic_array_try_from_slice()

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use clap::Parser;
 use clap_stdin::FileOrStdin;
+use generic_array::{ArrayLength, GenericArray};
 use ios_log_decrypt::EncryptedLog;
 use log::error;
 use rpassword::prompt_password;
 use std::io::{stdout, Write};
 use std::process::exit;
-
-const KEY_SIZE: usize = 32;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -21,7 +20,7 @@ fn main() {
 
     let args = Args::parse();
     let encrypted_log = EncryptedLog::new(args.logfile.to_string());
-    let key: [u8; KEY_SIZE] = hex::decode(args.key.unwrap_or_else(|| {
+    let key = hex::decode(args.key.unwrap_or_else(|| {
         prompt_password("Decryption key: ").unwrap_or_else(|error| {
             error!("{error}");
             exit(1)
@@ -30,17 +29,29 @@ fn main() {
     .unwrap_or_else(|error| {
         error!("{error}");
         exit(2);
-    })
-    .try_into()
-    .unwrap_or_else(|vec: Vec<u8>| {
-        error!("Invalid key size: {}", vec.len());
-        exit(3);
     });
 
-    for block in encrypted_log.decrypt(&key.into()) {
+    for block in encrypted_log.decrypt(generic_array_try_from_slice(key.as_slice()).unwrap_or_else(
+        || {
+            error!("Invalid key size.");
+            exit(3);
+        },
+    )) {
         match block {
             Ok(ref bytes) => stdout().write_all(bytes).expect("could not write bytes"),
             Err(error) => error!("{error}"),
         }
+    }
+}
+
+/// Convert slices to `GenericArray`s without panicking.
+fn generic_array_try_from_slice<T, N>(data: &[T]) -> Option<&GenericArray<T, N>>
+where
+    N: ArrayLength<T>,
+{
+    if data.len() == N::to_usize() {
+        Some(data.into())
+    } else {
+        None
     }
 }
